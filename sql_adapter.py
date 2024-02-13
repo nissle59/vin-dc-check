dc2b648cff3cbc0e7b449e4c7bc41a73fd15de82
Mikhaylov Nikita <nixncom@gmail.com>
2024-02-13 13:56:35 +0300

    sql added

--- sql_adapter.py
+++ sql_adapter.py
@@ -46,7 +46,7 @@ def check_vin(vin: str):
     if v:
         print(v)
         dc = find_dc(v[1])
-        if dc[3] > convert_to_ts(datetime.datetime.now().strftime('%Y-%m-%d')):
+        if convert_to_ts(dc[3].strftime('%Y-%m-%d')) > convert_to_ts(datetime.datetime.now().strftime('%Y-%m-%d')):
             result = {
                 'source': 'cache',
                 'vin': v[0],
