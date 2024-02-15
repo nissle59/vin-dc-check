6b651b7938402d1577f733e10b8c0100e551881f
Mikhaylov Nikita <nixncom@gmail.com>
2024-02-15 15:03:01 +0300

    Restruct

--- sql_adapter.py
+++ sql_adapter.py
@@ -154,8 +154,8 @@ async def create_vin_act_dk(vin_d):
             for prev_dk in vin_d["previousDcs"]:
                 items_tuple = (prev_dk["dcNumber"], vin_d["body"], convert_to_ts(prev_dk["dcDate"]),
                                convert_to_ts(prev_dk["dcExpirationDate"]))
-                query = f"INSERT INTO dk_previous VALUES {items_tuple} ON CONFLICT DO NOTHING"
-                prev_data = await db.execute(query)
+                query = f"INSERT INTO dk_previous VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING"
+                prev_data = await db.execute(query, items_tuple)
             return True
         else:
             return None
