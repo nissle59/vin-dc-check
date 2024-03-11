2137957b9fe7d25b267b8042c5b39438d281e11b
Mikhaylov Nikita <nixncom@gmail.com>
2024-03-11 15:30:48 +0300

    Restruct

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 pip install -r /var/www/html/requirements.txt
 sh -c 'echo "" > $(docker inspect --format="{{.LogPath}}" parser_vin_dc_gibdd)'
-uvicorn server:app --host 0.0.0.0 --port 5000 --workers 5
+uvicorn server:app --host 0.0.0.0 --port 5000 --workers 15
 tail -f /dev/null
