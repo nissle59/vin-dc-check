dd5dc032129276ad34ec46805f59836a1f389190
Mikhaylov Nikita <nixncom@gmail.com>
2024-03-12 17:20:42 +0300

    Restruct

--- server.py
+++ server.py
@@ -279,7 +279,7 @@ async def upd_prx():
 @app.get("/bDc")
 async def qdc(vin):
     job = config.queue.enqueue(service.queue_dc, vin)
-    print(job.__dict__)
+    # print(job.__dict__)
 
     res = json.dumps(
         {"status": "success", "job": job.id, "jobCreatedAt": job.created_at},
