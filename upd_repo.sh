b42d14c2e074004b5f184377e30f2b2a3da691f1
Mikhaylov Nikita <nixncom@gmail.com>
2024-03-13 12:26:04 +0300

    Restruct

--- upd_repo.sh
+++ upd_repo.sh
@@ -1,2 +1,3 @@
 git pull
 docker restart parser_vin_dc_gibdd
+curl -X POST -F 'chat_id=288772431' -F 'text=continer VIN DC restarted' https://api.telegram.org/bot7194357846:AAGfBntMhRcfEpoHPJ0JiVMdXN12FYQUQ4g/sendMessage
