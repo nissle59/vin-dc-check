38454dc92b183391c42d58592345668563bba487
Mikhaylov Nikita <nixncom@gmail.com>
2024-04-24 08:37:22 +0300

    Restruct

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-pip install -r /var/www/html/requirements.txt > /dev/null 2>&1
-rm `$(docker inspect --format="{{.LogPath}}" parser_vin_dc_gibdd)`
+pip install -r /var/www/html/requirements.txt #> /dev/null 2>&1
+#rm `$(docker inspect --format="{{.LogPath}}" parser_vin_dc_gibdd)`
 chmod +x ./run_workers.sh
 #./run_workers.sh &
 uvicorn server:app --host 0.0.0.0 --port 5000 --workers 3
